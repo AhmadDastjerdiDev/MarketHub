3fd3802283290f1b51fb83a9202a6c2d5d1965af
Ahmad Dastjerdi <sahir68@gmail.com>
2025-03-28 15:36:12 +0000

    add getter, setter

--- src/main/java/com/example/markethub1/customer/entity/Customer.java
+++ src/main/java/com/example/markethub1/customer/entity/Customer.java
@@ -3,15 +3,17 @@ package com.example.markethub1.customer.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
 import java.util.List;
 
 import java.util.Date;
 
-
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tbl_customer")
 public class Customer {
