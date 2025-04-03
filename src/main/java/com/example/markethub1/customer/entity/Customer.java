b1df5e5232d202cf0b0a68017893e1b6170b6a18
Ahmad Dastjerdi <sahir68@gmail.com>
2025-04-03 15:13:13 +0100

    change in some basics

--- src/main/java/com/example/markethub1/customer/entity/Customer.java
+++ src/main/java/com/example/markethub1/customer/entity/Customer.java
@@ -1,8 +1,6 @@
 package com.example.markethub1.customer.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
@@ -17,17 +15,33 @@ import java.util.Date;
 @Entity
 @Table(name = "tbl_customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "c_id")
     private Long id;
+
+    @Column(name = "c_fullName")
     private String fullName;
+
+    @Column(name = "c_SNS")
     private String SNS;
+
+    @Column(name = "phoneMumber")
     private String phoneNumber;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "job")
     private String job;
+
+    @Column(name = "birthday")
     private Date birthday;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "postalCode")
     private String postalCode;
-    private List myFavorites;
-
-
 
 }
