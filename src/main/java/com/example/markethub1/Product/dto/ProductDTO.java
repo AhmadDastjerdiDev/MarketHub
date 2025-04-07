05b99105ba6bc27180f5f0f51f5cd22ba42981b4
Ahmad Dastjerdi <ahmaddastjerdi.eng@gmail.com>
2025-04-07 19:14:31 +0100

    add dto classes

---
+++ src/main/java/com/example/markethub1/Product/dto/ProductDTO.java
@@ -0,0 +1,24 @@
+package com.example.markethub1.Product.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDTO {
+
+        private Long productId;
+
+        private String productName;
+
+        private String productType;
+
+        //private String producer;
+
+        private Long price;
+}
---
+++ src/main/java/com/example/markethub1/order/dto/OrderDTO.java
@@ -0,0 +1,27 @@
+package com.example.markethub1.order.dto;
+
+import com.example.markethub1.customer.entity.Customer;
+import lombok.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+        private Long orderId;
+
+        private Date date;
+
+        private Integer orderCode;
+
+        private Long totalPayable;
+
+        private Customer receiver;
+
+        private Long shippingCost;
+
+        private Integer score;
+}
